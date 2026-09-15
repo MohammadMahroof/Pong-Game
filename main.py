8a70b3419a06c56746d6cf2ff4a78acdaa9a84a8
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-15 13:23:38 +0530

    feat: create paddle class with vertical movement

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(800,600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+paddle = Paddle()
+
+
+screen.listen()
+screen.onkey(paddle.go_up, "Up")
+screen.onkey(paddle.go_down, "Down")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 
 
