3eeff65605e198e82ba88a97030dfe9f61fe7d83
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-16 15:55:57 +0530

    feat: add ball-paddle collision detection

--- main.py
+++ main.py
@@ -9,24 +9,32 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-r_paddle = Paddle((-350, 0))
-l_paddle = Paddle((350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
 
 
 screen.listen()
-screen.onkey(l_paddle.go_up, "Up")
-screen.onkey(l_paddle.go_down, "Down")
-screen.onkey(r_paddle.go_up, "w")
-screen.onkey(r_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
-    time.sleep(0.5)
+    time.sleep(0.1)
     screen.update()
     ball.move()
+
+    #Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    #distance() -> How close is the ball to the paddle
+    #xcor() -> Has the ball reached the correct side
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
         
 
 
